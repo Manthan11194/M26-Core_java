281b005cff8dd4221912af3342a57b71385cdf76
Manthan11194 <jadhavmanthan2001@gmail.com>
2022-10-17 22:53:02 +0530

    Second Commit

---
+++ BasicJavaMathPrograms/src/org/tnsindia/in/Secound_Mul_Program.java
@@ -0,0 +1,24 @@
+package org.tnsindia.in;
+
+import java.util.Scanner;
+
+public class Secound_Mul_Program {
+
+	public static void main(String[] args) {
+
+		Scanner obj= new Scanner(System.in);
+
+		System.out.println("Please enter first Number-->"); 
+		
+		int num1=obj.nextInt();
+
+		System.out.println("Please enter second Number-->"); 
+		
+		int num2=obj.nextInt();
+
+		int mul=num1+num2; System.out.println("Sum of two Numbers-->"+mul);
+
+
+	}
+
+	}
