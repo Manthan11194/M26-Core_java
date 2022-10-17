4b6c9a94d4ccd760596791aa566d78b8a5ddd7af
Manthan11194 <jadhavmanthan2001@gmail.com>
2022-10-17 22:39:23 +0530

    fourth Commit

---
+++ ArrayDemo1/src/org/tnsindia/in/ArrayDemo.java
@@ -0,0 +1,11 @@
+package org.tnsindia.in;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+	short[] arr=new short[10];
+	System.out.println(arr.getClass().getName());
+
+	}
+
+}
