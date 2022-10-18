59ad3e294b904a547ff9f6b094dca1c042aa39e6
Manthan11194 <jadhavmanthan2001@gmail.com>
2022-10-18 11:07:18 +0530

    Second Commit

---
+++ Day2_Java_Maths_Program/src/org/tnsindia/in/Fourth_sub_program.java
@@ -0,0 +1,25 @@
+
+
+
+package org.tnsindia.in;
+import java.util.Scanner;
+public class Fourth_sub_program {
+
+	public static void main(String[] args) {
+		
+
+			Scanner obj= new Scanner(System.in);
+
+			System.out.println("Please enter first Number-->"); 
+			
+			int num1=obj.nextInt();
+
+			System.out.println("Please enter second Number-->"); 
+			
+			int num2=obj.nextInt();
+
+			int sub=num1*num2; System.out.println("Sum of two Numbers-->"+sub);
+
+		}
+
+		}
