c3693100df9cb581149e3253f56b4290fac8d229
Manthan11194 <jadhavmanthan2001@gmail.com>
2023-01-19 21:01:43 +0530

    Line Commit

---
+++ M26-Batch/ASSIGNMENT - 1/com/tns/Looping_Iteration/AssignmentQuestion1_Do_WhileLooping.java
@@ -0,0 +1,16 @@
+package com.tns.Looping_Iteration;
+
+public class AssignmentQuestion1_Do_WhileLooping {
+	
+		public static void main(String arg[])
+		{
+		    int n=1;
+		    //loop block start here
+		    do {
+		        System.out.println(n);
+		        n++;
+		    }while(n<=10);
+		//condition at the end in do-while
+		}
+	}
+
---
+++ M26-Batch/ASSIGNMENT - 1/com/tns/Looping_Iteration/For.java
@@ -0,0 +1,14 @@
+package com.tns.Looping_Iteration;
+
+	public class For{
+	
+		public static void main(String arg[])
+		{
+			int n;
+			//for loop start here
+			for(n=1; n<=10; n++)
+			{
+				System.out.println(n);
+			}
+	}
+}  
